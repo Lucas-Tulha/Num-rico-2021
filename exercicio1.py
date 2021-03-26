5455db67ef2dd7305e1d8812dedba04ec4fbcef3
Lucas Tulha <you@example.comlucastulhabr@gmail.com>
2021-03-26 16:45:54 -0300

    estou adicionando alguns arquivos

---
+++ exercicio1.py
@@ -0,0 +1,12 @@
+from matplotlib import *
+from numpy import *
+from metodos import *
+
+#Teste 1 de Runge-Kutta
+
+
+listaN = [20, 40, 80, 160, 320, 640]      # aqui sao inicializadas as entradas do usuario
+To = 0
+Tf = 2
+
+
---
+++ metodos.py
@@ -0,0 +1,23 @@
+from matplotlib import *
+from numpy import *
+
+
+#RUNGE-KUTTA, nessa parte vao ficar todos os metodos pedidos como de Runge-Kutta
+
+
+
+#Euler explicito, nessa parte vao ficar todos os metodos pedidos como de Euler explicito
+
+
+
+
+#Euler implicito, nessa parte vao ficar todos os metodos pedidos como de Euler implicito
+
+
+
+
+
+
+
+
+
